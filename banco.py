727576e15ab435ee9e04343588373579be806def
Valci Júnior <valcijunior19@hotmail.com>
2024-03-07 21:09:04 -0300

    Adicionado as funcionalidades de deposito, saque e imprimir extrato

--- banco.py
+++ banco.py
@@ -17,12 +17,34 @@ while True:
     opcao = int(input(menu))
 
     if opcao == 1:
-        pass
+        deposito = float(input('Digite o valor do depósito:R$ '))
+        if deposito <= 0:
+            print(f'Você tentou depositar R${deposito:.2f}, não é permitido valores menores ou igual a zero.\nDigite um valor válido.')
+        else:
+            saldo += deposito
+            extrato += f'=> Depósito R${deposito:.2f}\n'
 
     elif opcao == 2:
-        pass
+
+        valor_saque = float(input('Digite o valor que deseja sacar:R$ '))
+
+        if valor_saque > saldo:
+            print(f'Você não possui saldo suficiente para sacar.\nSaldo R${saldo:.2f}')
+        elif valor_saque > limite:
+            print(f'Operação Inválida. Valor de saque maior do que o limite.\nLimite R${limite:.2f}')
+        elif numero_saques >= LIMITE_SAQUES:
+            print(f'Você ultrapassou o limite de saques diários.\nLimite de saques {LIMITE_SAQUES}')
+
+        elif valor_saque > 0:
+            saldo -= valor_saque
+            extrato += f'=> Saque: R$-{valor_saque:.2f}\n'
+            numero_saques += 1
+
     elif opcao == 3:
-        pass
+        print('###### Extrato ######')
+        print('Não foram realizadas movimentações!!' if not extrato else extrato)
+        print(f'\nSaldo: R${saldo:.2f}')
+        print('#####################')
 
     elif opcao == 0:
         print('Muito obrigado pela preferência. É um prazer tê-lo conosco\nVolte Sempre!!')
